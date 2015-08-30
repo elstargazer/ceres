2ae1ef906d0bd2d762b4fd6915547362c652a7bc
AntonErmakov <anton.i.ermakov@gmail.com>
2015-08-30 16:06:24 -0400

    added Files structure

--- matlab_src/PowerLawMeshTest.m
+++ matlab_src/PowerLawMeshTest.m
@@ -1,11 +1,20 @@
 ccc
-matlab_config_filename   = '~/Dawn/FE/config/ConfigurationMatlab.cfg';
-config_template_filename = '~/Dawn/FE/config/ConfigurationTemplate.cfg';
+matlab_config_filename   = '../config/ConfigurationMatlab.cfg';
+config_template_filename = '../config/ConfigurationTemplate.cfg';
+config_list_filename     = '../RunList.txt';
+
+Files.matlab_config_filename = matlab_config_filename;
+Files.config_template_filename = config_template_filename;
+Files.config_list_filename = config_list_filename;
+
 Nmeshes   = 10; % number of meshes to be generated
 
 % Ceres topography spectrum power law parameters
 r_mean    = 470000;
 beta      = -3.72;
 intercept = 8.079;
-GeneratePowerLawMesh(matlab_config_filename,config_template_filename,...
-    r_mean,beta,intercept,Nmeshes);
+GeneratePowerLawMesh(Files,r_mean,beta,intercept,Nmeshes);
+
+
+
+
