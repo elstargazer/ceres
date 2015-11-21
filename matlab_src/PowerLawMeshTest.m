2733c14984662c6b84db9defef4083ae17129ad1
AntonErmakov <anton.i.ermakov@gmail.com>
2015-11-20 19:50:40 -0500

    added config file reading code

--- matlab_src/PowerLawMeshTest.m
+++ matlab_src/PowerLawMeshTest.m
@@ -1,20 +1,17 @@
 ccc
+
 matlab_config_filename   = '../config/ConfigurationMatlab.cfg';
-config_template_filename = '../config/ConfigurationTemplate.cfg';
+config_template_filename = '../config/ConfigurationV2.cfg';
 config_list_filename     = '../RunList.txt';
 
-Files.matlab_config_filename = matlab_config_filename;
+Files.matlab_config_filename   = matlab_config_filename;
 Files.config_template_filename = config_template_filename;
-Files.config_list_filename = config_list_filename;
+Files.config_list_filename     = config_list_filename;
 
-Nmeshes   = 10; % number of meshes to be generated
+Nmeshes   = 1; % number of meshes to be generated
 
 % Ceres topography spectrum power law parameters
-r_mean    = 470000;
-beta      = -3.72;
-intercept = 8.079;
-GeneratePowerLawMesh(Files,r_mean,beta,intercept,Nmeshes);
 
+cfg = ReadConfig(Files);
 
-
-
+GeneratePowerLawMesh2(Files,cfg,Nmeshes);
