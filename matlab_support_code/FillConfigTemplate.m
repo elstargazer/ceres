7d1a1f6246dc121050708bd6062e4e80cfc954b8
AntonErmakov <anton.i.ermakov@gmail.com>
2016-02-19 16:51:02 -0500

    added folder str

--- matlab_support_code/FillConfigTemplate.m
+++ matlab_support_code/FillConfigTemplate.m
@@ -1,9 +1,12 @@
-function FillConfigTemplate(config_template_filename,mesh_filename,append)
+function new_complete_path = FillConfigTemplate(config_template_filename,mesh_filename,append,runname)
 
 [path,name,ext] = fileparts(config_template_filename);
 
+mkdir([path '/' runname]);
+new_complete_path = [path '/' runname '/' name '_' append ext];
+
 in_old = fopen(config_template_filename,'r');
-in_new = fopen([path '/' name '_' append ext],'w');
+in_new = fopen(new_complete_path,'w');
 
 str = fgetl(in_old);
 fprintf(in_new,'%s',str);
@@ -15,9 +18,8 @@ while (str~=-1)
     if strncmpi(str,key_mesh,numel(key_mesh))    
         fprintf(in_new,[key_mesh ' = "' mesh_filename(4:end) '";\n']);
     elseif strncmpi(str,key_out,numel(key_out))
-            fprintf(in_new,[key_out ' = "output/119/output_' append '/";\n']);
-            mkdir(['../output/output_' append])
-
+            fprintf(in_new,[key_out ' = "output/' runname '/output_' append '/";\n']);
+            mkdir(['../output/' runname '/output_' append])
     else
         fprintf(in_new,'%s\n',str);
         
@@ -27,3 +29,4 @@ end
 
 fclose(in_old);
 fclose(in_new);
+
