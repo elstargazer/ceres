588261acc6b57b5ce1a41b227c21ed157dcba78c
AntonErmakov <anton.i.ermakov@gmail.com>
2016-02-19 16:53:43 -0500

    added runname

--- matlab_src/PowerLawMeshTest.m
+++ matlab_src/PowerLawMeshTest.m
@@ -1,17 +1,18 @@
 ccc
 
 matlab_config_filename   = '../config/ConfigurationMatlab.cfg';
-config_template_filename = '../config/ConfigurationV2.cfg';
+config_template_filename = '../config/Run115.cfg';
 config_list_filename     = '../RunList.txt';
+runname                  = 'run115';
 
 Files.matlab_config_filename   = matlab_config_filename;
 Files.config_template_filename = config_template_filename;
 Files.config_list_filename     = config_list_filename;
 
-Nmeshes   = 24; % number of meshes to be generated
+Nmeshes   = 3; % number of meshes to be generated
 
 % Ceres topography spectrum power law parameters
 
 cfg = ReadConfig(Files);
 
-GeneratePowerLawMesh2(Files,cfg,Nmeshes);
+GeneratePowerLawMesh2(Files,cfg,Nmeshes,runname);
